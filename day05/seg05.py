6a86899ca9c6215bffd3e2c51db71d7a1cd05481
hyeily0627 <hj_o5h@naver.com>
2024-06-26 13:48:42 +0900

    Update seg05.py

--- day05/seg05.py
+++ day05/seg05.py
@@ -1,22 +1,51 @@
 import RPi.GPIO as GPIO
 import time
 
-switch = 25
-com_pins = [20, 21, 22, 23]
-led_pin = [4, 5, 6, 12, 13, 16, 17]
-# a 4(빨강), b 5(갈색), c 6(초록)
-#d 12(파랑), e 13(회색), f 16(검정), g 17(흰색) 
+segment_pins = [24, 5, 6, 12, 13, 16, 17]
+digit_pins = [20, 21, 22, 23]
 
-# GPIO 설정
-GPIO.setmode(GPIO.BCM)
-GPIO.cleanup()
+segment_patterns = [
+    [1, 1, 1, 1, 1, 1, 0],  # 0
+    [0, 1, 1, 0, 0, 0, 0],  # 1
+    [1, 1, 0, 1, 1, 0, 1],  # 2
+    [1, 1, 1, 1, 0, 0, 1],  # 3
+    [0, 1, 1, 0, 0, 1, 1],  # 4
+    [1, 0, 1, 1, 0, 1, 1],  # 5
+    [0, 0, 1, 1, 1, 1, 1],  # 6
+    [1, 1, 1, 0, 0, 1, 0],  # 7
+    [1, 1, 1, 1, 1, 1, 1],  # 8
+    [1, 1, 1, 0, 0, 1, 1]   # 9
+]
 
-GPIO.setup(switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    for pin in segment_pins:
+        GPIO.setup(pin, GPIO.OUT)
+    for pin in digit_ins:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.HIGH)  
 
-for ledPin in led_pin:
-	GPIO.setup(ledPin, GPIO.OUT)
-	GPIO.output(ledPin, True)
-	
-num = [[0,1,1,0,0,0,0],[1,1,0,1,1,0,1],[1,1,1,1,0,0,1],
-				[0,1,1,0,0,1,1],[1,0,1,1,0,1,1],[1,0,1,1,1,1,1],
-				[1,1,1,0,0,1,0],[1,1,1,1,1,1,1],[1,1,1,1,0,1,1]]
+def display_number(number):
+    digits = [int(d) for d in str(number).zfill(4)] 
+    for i in range(4):
+        GPIO.output(digit_pins[i], GPIO.LOW) 
+        pattern = segment_patterns[digits[i]]
+        for pin, state in zip(segment_pins, pattern):
+            GPIO.output(pin, state)
+        time.sleep(0.005) 
+        GPIO.output(digit_pins[i], GPIO.HIGH)  
+def main():
+    setup()
+    try:
+        for number in range(1, 10000):
+            start_time = time.time()
+            while time.time() - start_time < 0.5: 
+                display_number(number)
+    except KeyboardInterrupt:
+        print("")
+    finally:
+        GPIO.cleanup()
+
+if __name__ == '__main__':
+    main()
