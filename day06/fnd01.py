968c77b50eb9332040aa9a289e3740a51995012a
OH HYE JIN <hj_o5h@naver.com>
2024-06-27 10:19:52 +0900

    Create fnd01.py

---
+++ day06/fnd01.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as GPIO
+import time
+
+# 0~9까지 1byte 헥사(hex) 값 
+fndData = [ 0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f]
+fndSegs = [4, 5, 6, 12, 13, 16, 17]
+fndSels = [20, 21, 22, 23]
+
+GPIO.setmode(GPIO.BCM)
+for fndSeg in fndSegs:
+	GPIO.setup(fndSeg, GPIO.OUT)
+	GPIO.output(fndSeg, 0)
+
+for fndSel in fndSels:
+	GPIO.setup(fndSel, GPIO.OUT)
+	GPIO.output(fndSel, 1)
+
+def fndOut(): # 하나의 숫자 형태를 만드는 함수 
+	for i in range(0,7):
+		# 예를 들어 1을 띄우고 싶다면 원리는 아래와 같음
+		#GPIO.output(fndSegs[0],0)
+		#GPIO.output(fndSegs[1],1)
+		#GPIO.output(fndSegs[2],1)
+		#GPIO.output(fndSegs[3],0)
+		GPIO.output(fndSegs[i], fndDatas[0] & (0x01 << i))
+
+try:
+	while True:
+		for i in range(0,4):
+			GPIO.output(fndSels[i], 0) 	#fnd 선택
+			#GPIO.output(5,1) 5,6을 켜서 1만 뜨는 지 확인했음! 
+			#GPIO.output(6,1)
+
+			for j in range(0,10):
+				fndOut()
+				time.sleep(0.5)
+
+except KeyboardInterrupt:
+	GPIO.cleanup() 
