43bbf9c459ad0a4232c3dd8d5359e392c7557c4e
OH HYE JIN <hj_o5h@naver.com>
2024-06-28 14:32:14 +0900

    .

--- day07/ledcontrol.py
+++ day07/ledcontrol.py
@@ -6,25 +6,20 @@ import RPi.GPIO as GPIO
 form_class = uic.loadUiType("./ledcontrol.ui")[0]
 
 # GPIO 핀 번호 설정
-self.red_pin = 19
-self.green_pin = 13
-self.blue_pin = 6
+red_pin = 19
+green_pin = 13
+blue_pin = 6
 
 class WindowClass(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
-        # GPIO 핀 번호 설정
-        self.red_pin = 19
-        self.green_pin = 13
-        self.blue_pin = 6
-
         # GPIO 설정
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.red_pin, GPIO.OUT)
-        GPIO.setup(self.green_pin, GPIO.OUT)
-        GPIO.setup(self.blue_pin, GPIO.OUT)
+        GPIO.setup(red_pin, GPIO.OUT)
+        GPIO.setup(green_pin, GPIO.OUT)
+        GPIO.setup(blue_pin, GPIO.OUT)
 
         # 이벤트 함수 등록
         self.BtnOn.clicked.connect(self.BtnOnFunction)
@@ -34,27 +29,27 @@ class WindowClass(QMainWindow, form_class):
 
     def BtnOnFunction(self):
         print("불을 켭니다")
-        GPIO.output(self.red_pin, True)
-        GPIO.output(self.green_pin, True)
-        GPIO.output(self.blue_pin, True)
+        GPIO.output(red_pin, False)
+        GPIO.output(green_pin, False)
+        GPIO.output(blue_pin, False)
         
     def BtnRedFunction(self):
         print("빨간색 불이 켜집니다")
-        GPIO.output(self.red_pin, True)
-        GPIO.output(self.green_pin, False)
-        GPIO.output(self.blue_pin, False)
+        GPIO.output(red_pin, False)
+        GPIO.output(green_pin, True)
+        GPIO.output(blue_pin, True)
         
     def BtnBlueFunction(self):
         print("파란색 불이 켜집니다")
-        GPIO.output(self.red_pin, False)
-        GPIO.output(self.green_pin, False)
-        GPIO.output(self.blue_pin, True)
+        GPIO.output(red_pin, True)
+        GPIO.output(green_pin, True)
+        GPIO.output(blue_pin, False)
         
     def BtnGreenFunction(self):
         print("초록색 불이 켜집니다")
-        GPIO.output(self.red_pin, False)
-        GPIO.output(self.green_pin, True)
-        GPIO.output(self.blue_pin, False)
+        GPIO.output(red_pin, True)
+        GPIO.output(green_pin, False)
+        GPIO.output(blue_pin, True)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
