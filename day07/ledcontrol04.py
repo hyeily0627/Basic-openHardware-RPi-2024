dbb67f0206c8fbb35ed33a31ecf56b7091557510
OH HYE JIN <hj_o5h@naver.com>
2024-07-02 09:13:01 +0900

    2024-07-02

--- day07/ledcontrol04.py
+++ day07/ledcontrol04.py
@@ -15,7 +15,7 @@ class WindowClass(QMainWindow, form_class) :
       super().__init__()
       self.setupUi(self)
 
-      # 踰꾪듉 ?대┃ ?대깽???곌껐
+      
       self.Btn_ON.clicked.connect(self.btnOnFunction)
       self.Btn_OFF.clicked.connect(self.btnOffFunction)
 
@@ -24,49 +24,48 @@ class WindowClass(QMainWindow, form_class) :
       self.Btn_GREEN.clicked.connect(self.btnGreenFunction)
       self.Btn_WHITE.clicked.connect(self.btnWhiteFunction)
 
-      # GPIO 珥덇린???ㅼ젙
+      
       GPIO.setmode(GPIO.BCM)
       GPIO.setup(LED_RED, GPIO.OUT)
       GPIO.setup(LED_BLUE, GPIO.OUT)
       GPIO.setup(LED_GREEN, GPIO.OUT)
 
-   # 踰꾪듉 ?대깽???⑥닔
+   
    def btnOnFunction(self) :
-      print("LED ON CLICK!!")
+      print("LED가 활성화 되었습니다")
 
    def btnOffFunction(self) :
       GPIO.output(LED_RED, True)
       GPIO.output(LED_BLUE, True)
       GPIO.output(LED_GREEN, True)
       GPIO.cleanup()
-      print("LED OFF CLICK!!")
+      print("LED가 종료되었습니다")
 
    def btnRedFunction(self) :
       GPIO.output(LED_RED, False)
       GPIO.output(LED_BLUE, True)
       GPIO.output(LED_GREEN, True)
-      print("LED OFF CLICK!!")
+      print("빨간불이 켜졌습니다")
 
    def btnBlueFunction(self) :
       GPIO.output(LED_RED, True)
       GPIO.output(LED_BLUE, False)
       GPIO.output(LED_GREEN, True)
-      print("LED OFF CLICK!!")
+      print("파란불이 켜졌습니다")
 
    def btnGreenFunction(self) :
       GPIO.output(LED_RED, True)
       GPIO.output(LED_BLUE, True)
       GPIO.output(LED_GREEN, False)
-      print("LED OFF CLICK!!")
+      print("초록불이 켜졌습니다")
 
    def btnWhiteFunction(self) :
       GPIO.output(LED_RED, False)
       GPIO.output(LED_BLUE, False)
       GPIO.output(LED_GREEN, False)
-      print("LED OFF CLICK!!")
+      print("흰색불이 켜졌습니다")
 
 
-# 硫붿씤?⑥닔
 if __name__ == "__main__" :
    app = QApplication(sys.argv)
    myWindow = WindowClass()
